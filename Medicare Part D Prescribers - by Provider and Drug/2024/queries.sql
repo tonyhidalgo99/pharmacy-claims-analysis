3afc301068d3788af71178e60d82128278f88dc7
Tony Hidalgo <tony.hidalgo99@gmail.com>
2026-09-04 12:55:15 -0400

    Add cost-per-claim and total drug cost aggregation queries

--- Medicare Part D Prescribers - by Provider and Drug/2024/queries.sql
+++ Medicare Part D Prescribers - by Provider and Drug/2024/queries.sql
@@ -47,3 +47,58 @@ FROM claims
 WHERE Tot_Benes IS NULL
 ORDER BY Tot_Clms DESC
 LIMIT 20;
+
+--Determine percentage of records where Tot_Benes IS NULL
+SELECT
+	COUNT (*)
+FROM claims
+WHERE Tot_Benes IS NULL;
+
+--Determine total number of records
+SELECT
+	COUNT (*) 
+FROM claims;
+--54.5% of dataset has Tot_Benes IS NULL
+
+--Determine number of DISTINCT Prscrbr_Type
+SELECT		
+	COUNT (DISTINCT(Prscrbr_Type))
+FROM claims;
+--There are 182 DISTINCT specialties in dataset	
+
+--Determine percentage of Tot_Benes IS NULL by Prscrbr_Type
+SELECT
+	Prscrbr_Type,
+	COUNT (*) AS total_rows,
+	SUM (CASE WHEN Tot_Benes IS NULL THEN 1 ELSE 0 END) AS null_count
+FROM claims
+GROUP BY Prscrbr_Type
+ORDER BY null_count DESC;
+
+--Before running next query, verify that there are no records with 0 Tot_Clms
+SELECT
+	COUNT (*)
+FROM claims
+WHERE Tot_Clms = 0;
+
+--Determine cost_per_claim, which is Tot_Drug_Cst/Tot_Clms
+SELECT
+	Brnd_Name,
+	Gnrc_Name,
+	SUM (Tot_Drug_Cst),
+	SUM (Tot_Clms),
+	SUM(Tot_Drug_Cst)/SUM(Tot_Clms) AS cost_per_claim
+FROM claims
+GROUP BY Brnd_Name
+ORDER BY cost_per_claim DESC
+LIMIT 50;
+
+--Determine highest cost medications, not per claim
+SELECT
+	Brnd_Name,
+	Gnrc_Name,
+	SUM (Tot_Drug_Cst) AS Total_drug_cost
+FROM claims
+GROUP BY Brnd_Name
+ORDER BY Total_drug_cost DESC
+LIMIT 50;
